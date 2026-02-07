c6e1b5d6aa0b19aa1b96a223fa8fd159b18fe91f
spiderasasen <spiderasasen@gmail.com>
2026-02-07 16:44:40 -0500

    made the layout that i like for the home screen

--- game/src/pages/Home.jsx
+++ game/src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import {useState, useEffect} from 'react'
+import "../styles/home.css"
 
 function Home(){
     return(
-      <div>
+      <div class="start-screen">
           <h1>Do you know your programming languages?</h1>
           <p>This is  list of all programming languages that exist today.<br/>How many do you know?</p>
           <button>Begin the game</button>
