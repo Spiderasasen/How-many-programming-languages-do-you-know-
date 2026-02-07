4c0f279ed5d9c69956cf5d035429707407303ccf
spiderasasen <spiderasasen@gmail.com>
2026-02-07 16:30:08 -0500

    made the basic layout of the main hud

--- game/src/pages/Home.jsx
+++ game/src/pages/Home.jsx
@@ -3,7 +3,9 @@ import {useState, useEffect} from 'react'
 function Home(){
     return(
       <div>
-          <p>Hello World!</p>
+          <h1>Do you know your programming languages?</h1>
+          <p>This is  list of all programming languages that exist today.<br/>How many do you know?</p>
+          <button>Begin the game</button>
       </div>
     );
 }
