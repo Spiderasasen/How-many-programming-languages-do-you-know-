81526a97c33757aa8e107e4ebbbdf11787f70544
spiderasasen <spiderasasen@gmail.com>
2026-02-09 15:57:51 -0500

    testing that the code can read a image. now adding all the langauges to the json

--- game/src/pages/Game.jsx
+++ game/src/pages/Game.jsx
@@ -1,10 +1,13 @@
 import {useState, useEffect} from 'react'
 import "../styles/game.css"
+import Language from "../data/language.json"
+import Ada from "../assets/Ada.png"
 
 function Game(){
     // vars for the game
     const [streak, setStreak] = useState(0);
     const [score, setScore] = useState(0);
+    const [language, setLanguage] = useState(Language[0]);
 
     return(
         <div className="gameScreen">
@@ -16,7 +19,7 @@ function Game(){
 
             {/*main area where the game will be*/}
             <div>
-                <img alt={"This is where the image will be located at"}/>
+                <img src={Ada} alt={"ada"}/>
                 <div>
                     <button>answer 1</button>
                     <button>answer 2</button>
